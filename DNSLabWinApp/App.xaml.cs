f27696f407fb683ac3915cf2c3add500ea7d0ea5
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-03-06 00:24:58 +0330

    Make a bit better user experience in single instance

--- DNSLabWinApp/App.xaml.cs
+++ DNSLabWinApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dnslabwin;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -15,12 +16,52 @@ namespace DNSLabWinApp
     /// </summary>
     public partial class App : Application
     {
-        Mutex myMutex;
+        /// <summary>The event mutex name.</summary>
+        private const string UniqueEventName = "DNSLabEvent";
+
+        /// <summary>The unique mutex name.</summary>
+        private const string UniqueMutexName = "DNSLab";
+
+        /// <summary>The event wait handle.</summary>
+        private EventWaitHandle eventWaitHandle;
+
+        /// <summary>The mutex.</summary>
+        private Mutex mutex;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            myMutex = new Mutex(true, "DNSLab", out bool aIsNewInstance);
-            if (!aIsNewInstance)
-                App.Current.Shutdown();
+            bool isOwned;
+            this.mutex = new Mutex(true, "DNSLab", out isOwned);
+            this.eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            // So, R# would not give a warning that this variable is not used.
+            GC.KeepAlive(this.mutex);
+
+            if (isOwned)
+            {
+                // Spawn a thread which will be waiting for our event
+                var thread = new Thread(
+                    () =>
+                    {
+                        while (this.eventWaitHandle.WaitOne())
+                        {
+                            Current.Dispatcher.BeginInvoke(
+                                (Action)(() => ((MainWindow)Current.MainWindow).BringToForeground()));
+                        }
+                    });
+
+                // It is important mark it as background otherwise it will prevent app from exiting.
+                thread.IsBackground = true;
+
+                thread.Start();
+                return;
+            }
+
+            // Notify other instance so it could bring itself to foreground.
+            this.eventWaitHandle.Set();
+
+            // Terminate this instance.
+            this.Shutdown();
         }
 
         void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
--- DNSLabWinApp/Windows/MainWindow.xaml.cs
+++ DNSLabWinApp/Windows/MainWindow.xaml.cs
@@ -226,5 +226,20 @@ namespace dnslabwin
             e.Cancel = true;
             this.Hide();
         }
+
+        public void BringToForeground()
+        {
+            if (this.WindowState == WindowState.Minimized || this.Visibility == Visibility.Hidden)
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+            }
+
+            // According to some sources these steps gurantee that an app will be brought to foreground.
+            this.Activate();
+            this.Topmost = true;
+            this.Topmost = false;
+            this.Focus();
+        }
     }
 }
